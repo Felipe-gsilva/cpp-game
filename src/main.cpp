391f4d9e84c4ea3abc1106a25d2b2f6b5ebf2d1c
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-03-11 15:02:44 -0300

    added glut

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-#include <string>
 #include "GLFW/glfw3.h"
-
+#include <GL/freeglut.h>
 
 namespace app{
 class Application{
 public:
   int createWindow(){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH); 
+
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
     return 0;
   } 
   int render()
@@ -15,13 +29,16 @@ public:
   }
 
 private:
+  int argc;
+  char *argv[];
+  
 };
 }
 
 int main(int argc, char *argv[]) {
   app::Application app;
   app.createWindow();
-  
+
   return 0;
 }
 
