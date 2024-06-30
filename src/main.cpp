8c311119cc5372b6a22130563414df7014498b07
Felipe-gsilva <felipe-gsilva@protonmail.com>
2024-06-30 15:33:10 -0300

    changed structure added sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
-#include "main.hpp"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include <iostream>
 
-using namespace app;
+class main {
+private:
+public:
+  main(){
+    surface();
+    window();
+  }
 
-int main(int argc, char *argv[]) {
-  app::Application app;
-  app.createWindow();
-  app.render();
-  app.closeWindow();
-  return 0;
-}
+  int window() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+      std::cout << "error initializing SDL: %s\n" <<  SDL_GetError() << std::endl;
+    }
 
+    SDL_Window* window = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
+    while(1)
+      ;
+  }
+  int surface() {
+    return 0;
+  }
 
+private:
 
+};
