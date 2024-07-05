a8dd0e5d2f2960f11755bc987feae6a21b8c230e
Felipe-gsilva <felipe-gsilva@protonmail.com>
2024-07-05 13:45:28 -0300

    updated ambience object

--- src/AmbienceObject.cpp
+++ src/AmbienceObject.cpp
@@ -1,14 +1,17 @@
 #include "AmbienceObject.h"
 #include <SDL2/SDL.h>
+#include "Game.h"
+#include "util/TextureHandler.h"
 
 AmbienceObject::AmbienceObject(const LoaderParams* params) : GameObject(params) { }
 
 void AmbienceObject::draw() {
-  GameObject::draw();
+  Texture::TextureHandler::Instance()->draw(textureId, coord.getX(), coord.getY(), width, height, Application::Game::Instance()->getRenderer());
 }
 
 void AmbienceObject::update() {
   coord.setX(coord.getX() + 1);
+  currentFrame = 1; 
 }
 
 void AmbienceObject::clean() {
